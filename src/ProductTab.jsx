3982b8eb940b496729fe1b2c0a2150afd47dec4e
Subhadeep Chakraborty <subadeep.c@gmail.com>
2026-03-26 21:17:35 +0530

    adding files

---
+++ src/ProductTab.jsx
@@ -0,0 +1,15 @@
+import Product from "./Product.jsx";
+function ProductTab()
+{
+    let styles={display:"flex",flexWrap:"wrap",justifyContent:"center",alignItems:"center"};
+    return(
+        <div style={styles}>
+            <h1></h1>
+            <Product title="Logitech MX Master" idx={0}/>
+            <Product title="Apple Pencil (2nd Gen)" idx={1}/>
+            <Product title="Zebronice Zeb-transformer" idx={2}/>
+            <Product title="Petronics Toad 23" idx={3}/>
+        </div>
+    )
+}
+export default ProductTab;
